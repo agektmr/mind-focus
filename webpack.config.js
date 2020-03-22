2ba78170c619dea404acdbc7d53f31a0895016b3
Eiji Kitamura <agektmr@google.com>
2020-03-22 12:22:42 +0900

    Build for production

--- webpack.config.js
+++ webpack.config.js
@@ -5,69 +5,73 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 const src = path.join(__dirname, 'src');
 const dst = path.join(__dirname, 'dist');
 
-module.exports = [{
-  mode: 'production',
-  entry: path.join(src, 'styles', 'style.scss'),
-  output: {
-    path: dst,
-    filename: path.join('scripts', 'style-bundle.js')
-  },
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: path.join('styles', 'bundle.css')
-          }
-        },
-        { loader: 'extract-loader' },
-        { loader: 'css-loader' },
-        {
-          loader: 'sass-loader',
-          options: {
-            implementation: require('sass'),
-            sassOptions: {
-              includePaths: ['./node_modules']
+module.exports = (env, argv) => {
+  const PRODUCTION = argv.mode === 'production';
+  return [{
+    mode: PRODUCTION ? 'production' : 'development',
+    entry: path.join(src, 'styles', 'style.scss'),
+    output: {
+      path: dst,
+      filename: path.join('scripts', 'style-bundle.js')
+    },
+    devtool: !PRODUCTION ? 'inline-source-map' : false,
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: path.join('styles', 'bundle.css')
+            }
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                includePaths: ['./node_modules']
+              }
             }
           }
-        }
-      ]
-    }]
-  }
-}, {
-  mode: 'production',
-  entry: {
-    'scripts/bundle': path.join(src, 'scripts', 'main.ts')
-  },
-  output: {
-    path: dst,
-    filename: '[name].js'
-  },
-  module: {
-    rules: [{
-      test: /\.ts$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }]
-  },
-  resolve: {
-    extensions: [ '.ts', '.js' ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([{
-      from: path.join(src, 'index.html'),
-      to: path.join(dst, 'index.html')
-    }, {
-      from: path.join(src, 'manifest.json'),
-      to: path.join(dst, 'manifest.json')
-    }, {
-      from: path.join(src, 'images'),
-      to: path.join(dst, 'images')
-    }]),
-    new WorkboxPlugin.InjectManifest({
-      swSrc: path.join(src, 'sw.js')
-    })
-  ]
-}];
+        ]
+      }]
+    }
+  }, {
+    mode: PRODUCTION ? 'production' : 'development',
+    entry: {
+      'scripts/bundle': path.join(src, 'scripts', 'main.ts')
+    },
+    output: {
+      path: dst,
+      filename: '[name].js'
+    },
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }]
+    },
+    resolve: {
+      extensions: [ '.ts', '.js' ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: path.join(src, 'index.html'),
+        to: path.join(dst, 'index.html')
+      }, {
+        from: path.join(src, 'manifest.json'),
+        to: path.join(dst, 'manifest.json')
+      }, {
+        from: path.join(src, 'images'),
+        to: path.join(dst, 'images')
+      }]),
+      new WorkboxPlugin.InjectManifest({
+        swSrc: path.join(src, 'sw.js')
+      })
+    ]
+  }];
+}
